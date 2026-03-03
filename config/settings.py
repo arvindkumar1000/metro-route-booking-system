f8e80383ee989c0a9d0268103a19b6d6095ce3f1
arvindkumar1000 <arvindnagar58955@gmail.com>
2026-03-03 14:27:04 +0530

    added safe fallback

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-4^$3u1n2z9^7melold^#ph&m0_j3rw!1(&lv$!lwllf^9h0(w('
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
